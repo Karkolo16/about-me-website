4cc5f569a3ad97a75bce8ccb808cb8062b4f2891
Karolina <ka.kolomyjec@gmail.com>
2023-02-16 17:24:53 +0100

    change navbar

--- js/script.js
+++ js/script.js
@@ -1,1 +1,7 @@
 'use strict'
+const toggleButton = document.getElementsByClassName('toggler')[0]
+const navbarLinks = document.getElementsByClassName('navbar-links')[0]
+
+toggleButton.addEventListener('click', () =>{
+    navbarLinks.classList.toggle('active')
+})

c1f3067757e5bea982fc96e09e0550c2fed33ce0
Karolina <ka.kolomyjec@gmail.com>
2023-03-23 18:33:07 +0100

    add js to th button menu

--- js/script.js
+++ js/script.js
@@ -1,2 +1,8 @@
 "use strict";
+
 const menuBtn = document.querySelector(".menu-btn");
+const navLinks = document.querySelector(".nav-links");
+
+menuBtn.addEventListener("click", () => {
+  navLinks.classList.toggle("mobile-menu");
+});
